e73ce87b921d96820b78e4b080c10dc19f9019bf
yy <1023767856@qq.com>
2019-05-08 17:55:52 +0800

    change a bug in start_thread

--- lib/common.py
+++ lib/common.py
@@ -9,9 +9,10 @@ class CommonFunc(object):
         pass
 
     @staticmethod
-    def start_thread(data, fun, max_worker=15, **kwargs):
+    def start_thread(data, fun, max_worker=15, is_test=False, **kwargs):
         """
         This function can start a thread pool
+        :param is_test:
         :param max_worker:
         :param data:
         :param fun:
@@ -24,7 +25,8 @@ class CommonFunc(object):
         for item in data:
             task = thread_pool.submit(fun, item, **kwargs)
             task_list.append(task)
-            break
+            if is_test:
+                break
         for i in as_completed(task_list):
             result.append(i.result())
         return result
